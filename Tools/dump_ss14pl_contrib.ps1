f5e4ec889140d3f91a9478312bb24dcec8c41168
nikitosych <boriszyn@gmail.com>
2025-09-04 01:55:12 +0200

    Refaktoryzacja

--- Tools/dump_ss14pl_contrib.ps1
+++ Tools/dump_ss14pl_contrib.ps1
@@ -5,68 +5,46 @@ $scriptDir = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 
 if ($null -eq $env:GITHUB_TOKEN)
 {
-    throw "A GitHub API token is required to run this script properly without being rate limited. If you're a user, generate a personal access token and use that. If you're running this in a GitHub action, make sure you expose the GITHUB_TOKEN secret as an environment variable."
+    throw "A GitHub API token is required to run this script properly. Make sure you expose the GITHUB_TOKEN secret as an environment variable."
 }
 
 function load_contribs([string] $repo)
 {
-    $qParams = @{
-        "per_page" = 100
-        "anon" = 1
-    }
-
     $headers = @{
-        Authorization="Bearer $env:GITHUB_TOKEN"
+        Authorization = "Bearer $env:GITHUB_TOKEN"
+        Accept        = "application/vnd.github.v3+json"
     }
 
-    $url = "https://api.github.com/repos/{0}/contributors" -f $repo
-    $r = @()
+    $url = "https://api.github.com/repos/$repo/stats/contributors"
 
-    while ($null -ne $url)
-    {
-        $resp = Invoke-WebRequest $url -Body $qParams -Headers $headers
-        $url = $resp.RelationLink.next
-        $j = ConvertFrom-Json $resp.Content
-        $r += $j
+    $maxRetries = 10
+    $attempt = 0
+    do {
+        $resp = Invoke-WebRequest -Uri $url -Headers $headers
+        $json = ConvertFrom-Json $resp.Content
+        Start-Sleep -Seconds 2
+        $attempt++
+    } while (($json -eq $null -or $json.Count -eq 0) -and $attempt -lt $maxRetries)
+
+    if ($json -eq $null -or $json.Count -eq 0) {
+        Write-Warning "No contributor data returned by Stats API"
+        return @()
     }
 
-    foreach ($contributor in $r)
-    {
-        if ($null -ne $contributor.name `
-            -And $null -ne $contributor.email `
-            -And $contributor.email -match '\d+\+(.*)@users\.noreply\.github\.com$')
-        {
-            $username = $Matches.1
-            if ($contributor.name.ToLower() -eq $username) {
-                $username = $contributor.name
-            }
-            if (($r).login -contains $username) { continue }
-            $contributor | Add-Member -MemberType NoteProperty -Name "login" -Value $username
-        }
-        elseif ($null -eq $contributor.login `
-                 -And $null -ne $contributor.name `
-                 -And !$contributor.name.Contains(" "))
-        {
-            $username = $contributor.name
-            if (($r).login -contains $username) { continue }
-            $userUrl = "https://api.github.com/users/{0}" -f $username
-            try {
-                $userResp = Invoke-WebRequest $userUrl -Headers $headers
-                $userJ = ConvertFrom-Json $userResp.Content
-                $contributor | Add-Member -MemberType NoteProperty -Name "login" -Value $userJ.login
-            }
-            catch {}
-        }
-    }
+    $logins = $json | ForEach-Object {
+        if ($_.author -ne $null) { $_.author.login }
+    } | Sort-Object -Unique
 
-    return $r
+    return $logins
 }
 
-$poloniumJson = load_contribs("polonium14/polonium-station")
+$poloniumContributors = load_contribs("polonium14/polonium-station")
 
-($poloniumJson).login + ($add) `
-    | select -unique `
+$result = $poloniumContributors + ($add) `
+    | Select-Object -Unique `
     | Where-Object { -not $ignore[$_] } `
-    | ForEach-Object { if($replacements[$_] -eq $null){ $_ } else { $replacements[$_] }} `
-    | Sort-object `
+    | ForEach-Object { if ($replacements[$_] -eq $null) { $_ } else { $replacements[$_] } } `
+    | Sort-Object `
     | Join-String -Separator ", "
+
+Write-Output $result
